471a916421784b206093a3a9fda68b9fecbe2997
Timothee Denizou <timothee.denizou@epita.fr>
2023-08-18 14:12:15 +0200

    weird directplay callback

--- src/Win95/DirectX.h
+++ src/Win95/DirectX.h
@@ -3,9 +3,13 @@
 
 #include "types.h"
 
+#define DirectPlay_EnumConnectionsCallback_ADDR (0x00487370)
+
 #define DirectX_DirectDrawEnumerateA_ADDR (0x0049D390)
 #define DirectX_DirectDrawCreate_ADDR (0x0049D396)
 #define DirectX_DirectInputCreateA_ADDR (0x0049D39C)
+
+BOOL DirectPlay_EnumConnectionsCallback(GUID* lpguidSP, LPVOID lpConnection, DWORD dwConnectionSize, LPCDPNAME lpName, DWORD dwFlags, LPVOID lpContext);
 
 HRESULT DirectX_DirectDrawEnumerateA(LPDDENUMCALLBACKA lpCallback, LPVOID lpContext);
 HRESULT DirectX_DirectDrawCreate(GUID* lpGuid, LPDIRECTDRAW* lplpDD, IUnknown* pUnkOuter);
