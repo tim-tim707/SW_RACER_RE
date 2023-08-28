2310698ed9dfa7fd2c677cbd9e4c6923e1f380cc
Timothee Denizou <timothee.denizou@epita.fr>
2023-08-28 16:56:49 +0200

    stdfnames done

--- src/General/stdFnames.c
+++ src/General/stdFnames.c
@@ -1,5 +1,59 @@
 #include "stdFnames.h"
 
+// 0x004845b0
+char* stdFnames_FindMedName(char* path)
+{
+    char* pcVar1;
+    char* res;
+
+    res = _strrchr(path, '\\');
+
+    if (res != NULL)
+    {
+        while ((res)[1] == '\\')
+        {
+            res = res + 1;
+        }
+        return res;
+    }
+    return path;
+}
+
+// 0x004845e0
+char* stdFnames_FindExt(char* path)
+{
+    char* result = _strrchr(stdFnames_FindMedName(path), '.');
+    if (result == NULL)
+    {
+        return NULL;
+    }
+    return result + 1;
+}
+
+// 0x00484600
+int stdFnames_ChangeExt(char* str, char* ext)
+{
+    char* result = stdFnames_FindExt(str);
+    if (result != NULL)
+    {
+        return NULL;
+    }
+    _strcat(str, ".");
+    _strcat(str, ext);
+    return 1;
+}
+
+// 0x00484670
+char* stsdFnames_StripExtAndDot(char* str)
+{
+    char* result = stdFnames_FindExt(str);
+    if (result != NULL)
+    {
+        result[-1] = 0;
+    }
+    return result;
+}
+
 // 0x00484690
 char* stdFnames_Concat(char* left, char* right, int bufferLen)
 {
--- src/General/stdFnames.h
+++ src/General/stdFnames.h
@@ -3,12 +3,18 @@
 
 #include "types.h"
 
+#define stdFnames_FindMedName_ADDR (0x004845b0)
+#define stdFnames_FindExt_ADDR (0x004845e0)
+#define stdFnames_ChangeExt_ADDR (0x00484600)
+#define stdFnames_StripExtAndDot_ADDR (0x00484670)
 #define stdFnames_Concat_ADDR (0x00484690)
-
 #define stdFnames_MakePath_ADDR (0x004846e0)
 
+char* stdFnames_FindMedName(char* path);
+char* stdFnames_FindExt(char* path);
+int stdFnames_ChangeExt(char* str, char* ext);
+char* stdFnames_StripExtAndDot(char* str);
 char* stdFnames_Concat(char* left, char* right, int bufferLen);
-
 void stdFnames_MakePath(char* str, int bufferLen, char* str2, char* extension);
 
 #endif // STDFNAMES_H
