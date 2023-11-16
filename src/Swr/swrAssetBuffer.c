4619bc9339d99af3336d7a1cb3a098ac2af7d94f
Timothee Denizou <timothee.denizou@epita.fr>
2023-11-16 21:08:59 +0100

    asset buffer functions

--- src/Swr/swrAssetBuffer.c
+++ src/Swr/swrAssetBuffer.c
@@ -13,3 +13,21 @@ char* swrAssetBuffer_GetBuffer(void)
 {
     return (&assetBuffer)[assetBufferIndex];
 }
+
+// 0x00445b50
+bool swrAssetBuffer_InBounds(char* ptr)
+{
+    char* buf;
+
+    buf = swrAssetBuffer_GetBuffer();
+    return ptr < buf;
+}
+
+// 0x00445bf0
+int swrAssetBuffer_RemainingSize(void)
+{
+    char* tmp;
+
+    tmp = swrAssetBuffer_GetBuffer();
+    return (int)assetBufferEnd - (int)tmp;
+}
--- src/Swr/swrAssetBuffer.h
+++ src/Swr/swrAssetBuffer.h
@@ -5,8 +5,14 @@
 
 #define swrAssetBuffer_SetBuffer_ADDR (0x00445b20)
 #define swrAssetBuffer_GetBuffer_ADDR (0x00445b40)
+#define swrAssetBuffer_InBounds_ADDR (0x00445b50)
 
-char* swrAssetBuffer_SetBuffer(char* buffer);
+#define swrAssetBuffer_RemainingSize_ADDR (0x00445bf0)
+
+void swrAssetBuffer_SetBuffer(char* buffer);
 char* swrAssetBuffer_GetBuffer(void);
+bool swrAssetBuffer_InBounds(char* ptr);
+
+int swrAssetBuffer_RemainingSize(void);
 
 #endif // SWRASSETBUFFER_H
