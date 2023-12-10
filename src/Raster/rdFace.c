7e91fc2f50399464c01a1ed52a860c0121e411c8
Timothee Denizou <timothee.denizou@epita.fr>
2023-12-10 02:07:29 +0100

    rdFace

--- src/Raster/rdFace.c
+++ src/Raster/rdFace.c
@@ -2,6 +2,28 @@
 
 #include "globals.h"
 
+// 0x004923c0
+int rdFace_NewEntry(RdFace* pFace)
+{
+    pFace->num = 0;
+    pFace->flags = 0;
+    pFace->numVertices = 0;
+    pFace->aVertIdxs = NULL;
+    pFace->aTexIdxs = NULL;
+    pFace->pMaterial = NULL;
+    pFace->matCelNum = -1;
+    (pFace->normal).x = 0.0;
+    (pFace->normal).y = 0.0;
+    (pFace->normal).z = 0.0;
+    (pFace->texVertOffset).x = 0.0;
+    (pFace->texVertOffset).y = 0.0;
+    (pFace->extraLight).x = 0.0;
+    (pFace->extraLight).y = 0.0;
+    (pFace->extraLight).z = 0.0;
+    (pFace->extraLight).w = 0.0;
+    return 1;
+}
+
 // 0x00492410
 void rdFace_FreeEntry(RdFace* pFace)
 {
--- src/Raster/rdFace.h
+++ src/Raster/rdFace.h
@@ -3,8 +3,10 @@
 
 #include "types.h"
 
+#define rdFace_NewEntry_ADDR (0x004923c0)
 #define rdFace_FreeEntry_ADDR (0x00492410)
 
+int rdFace_NewEntry(RdFace* pFace);
 void rdFace_FreeEntry(RdFace* pFace);
 
 #endif // RDFACE_H
