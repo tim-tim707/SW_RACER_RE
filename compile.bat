a941ce4ac5adb649bf59299194851689d3bb0b2b
Deon Poncini <deonponcini@gmail.com>
2024-03-19 20:36:53 -0700

    Cleanup generated code
    All generated code is moved to src/generated, which can house this
    generated code without the need for polluting the top level src directly
    
    All jinja templates move to src/templates for this generation. Update
    CMakeLists and compile.bat to use these new generated files

--- compile.bat
+++ compile.bat
@@ -9,9 +9,9 @@ cd ..
 python3.11 scripts\GenerateGlobalHeaderFromSymbols.py
 python3.11 scripts\GenerateHooks.py
 
-set SOURCES=src\dllMain.c src\main.c src\hook.c src\swr.c src\hook_generated.c src\stdPlatform.c src\Dss\*.c src\Engine\*.c src\General\*.c src\Gui\*.c src\Main\*.c src\Platform\*.c src\Primitives\*.c src\Raster\*.c src\Unknown\*.c src\Swr\*.c src\Win95\*.c
+set SOURCES=src\dllMain.c src\main.c src\hook.c src\swr.c src\generated\hook_generated.c src\stdPlatform.c src\Dss\*.c src\Engine\*.c src\General\*.c src\Gui\*.c src\Main\*.c src\Platform\*.c src\Primitives\*.c src\Raster\*.c src\Unknown\*.c src\Swr\*.c src\Win95\*.c
 set FLAGS=-s -shared -DINCLUDE_DX_HEADERS=1 -Wall -Wextra -Wno-unused-parameter -Wno-unused-variable
-set INCLUDES=-I. -Isrc
+set INCLUDES=-I. -Isrc -Isrc/generated
 set LIBS=-lgdi32 -lcomctl32 -lole32 -lwinmm
 
 echo Compiling... This might take a moment
