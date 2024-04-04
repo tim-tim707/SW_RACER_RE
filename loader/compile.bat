aac610d669dd44546b02bd93aa6693c9d5a88ed9
Timothee Denizou <timothee.denizou@epita.fr>
2024-04-04 23:55:47 +0200

    removed loader and mods/QOL and moved them into PitDroidModManager repository

--- loader/compile.bat
+++
@@ -1,4 +0,0 @@
-set SOURCES=loader.cpp md5.c
-set FLAGS=-g -Wall -Wextra -pedantic
-
-g++ -o loader %SOURCES% %FLAGS%
--- mods/QualityOfLife/compile.bat
+++
@@ -1,9 +0,0 @@
-set SOURCES=dllMain.c config.c
-set FLAGS=-shared -g -Wall -Wextra -pedantic
-set INCLUDES=
-set LIBS=-lgdi32 -lcomctl32
-
-g++ -o swr_reimpl.dll %SOURCES% %FLAGS% %INCLUDES% %LIBS%
-
-move swr_reimpl.dll "C:\Users\Tim\Desktop\STAR WARS Racer - Copy"
-echo No | copy /-Y Loader_config.txt "C:\Users\Tim\Desktop\STAR WARS Racer - Copy"
