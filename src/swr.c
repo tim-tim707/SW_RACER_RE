79c675667c7cd4242edfa3e4d920cc92781c1897
tly000 <>
2024-04-06 16:39:10 +0200

    bugfixes

--- src/swr.c
+++ src/swr.c
@@ -45,19 +45,3 @@ void swr_noop3(void)
 {
     swr_noop1();
 }
-
-// 0x00483A90
-void sub_483A90(int x)
-{
-    HANG("TODO");
-}
-
-// 0x00409510
-void sub_409510(int a1, int a2)
-{
-    HANG("TODO");
-}
-
-// 0x004502B0 HOOK
-void sub_4502B0()
-{}
