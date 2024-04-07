c27d9465ad6ef3478f5fad8b6af22d8d8c25b4f7
tly000 <>
2024-04-07 14:34:14 +0200

    ida scripts bugfix

--- scripts/ida/import_all.py
+++ scripts/ida/import_all.py
@@ -8,6 +8,34 @@ import ida_auto
 for (func, addr) in function_addresses.items():
     idc.set_name(addr, func)
 
+
+clang_argv = [
+    "-target i686-pc-windows-gnu",
+    "-x c",
+    "-I", src_dir,
+    "-I", src_dir + "/generated/"
+    "-mno-sse",
+    "-fsyntax-only",
+    "--sysroot", "C:/mingw32",
+    "-isystem", "C:/mingw32/lib/clang/12.0.0/include",
+    "-isystem", "C:/mingw32/lib/gcc/i686-w64-mingw32/13.2.0/include"
+]
+ida_srclang.set_parser_argv("clang", " ".join(clang_argv))
+
+decl = "\n".join([f'#include "{h}"' for h in headers])
+
+ida_srclang.parse_decls_with_parser("clang", None, decl, False)
+
+for (name, props) in data_symbols.items():
+    (addr, type) = props
+    idc.set_name(addr, name)
+    if type == "char":
+        type = "uint8_t" # TODO hack: ida creates arrays out of "char" types automatically...
+
+    decl = idc.parse_decl(type, idc.PT_SILENT)
+    idc.apply_type(addr, decl, idc.TINFO_DEFINITE)
+
+
 # TODO total hack...
 for h in headers:
     if not h in function_names_per_file:
@@ -34,26 +62,3 @@ for h in headers:
                 print(f"warning: could not parse decl \"{line}\"")
 
             idc.apply_type(function_addresses[names[0]], decl, idc.TINFO_GUESSED)
-
-
-clang_argv = [
-    "-target i686-pc-windows-gnu",
-    "-x c",
-    "-I", src_dir,
-    "-mno-sse",
-    "-fsyntax-only",
-    "--sysroot", "C:/mingw32",
-    "-isystem", "C:/mingw32/lib/clang/12.0.0/include",
-    "-isystem", "C:/mingw32/lib/gcc/i686-w64-mingw32/13.2.0/include"
-]
-ida_srclang.set_parser_argv("clang", " ".join(clang_argv))
-
-decl = "\n".join([f'#include "{h}"' for h in headers])
-
-ida_srclang.parse_decls_with_parser("clang", None, decl, False)
-
-for (name, props) in data_symbols.items():
-    (addr, type) = props
-    idc.set_name(addr, name)
-    decl = idc.parse_decl(type, idc.PT_SILENT)
-    idc.apply_type(addr, decl)
