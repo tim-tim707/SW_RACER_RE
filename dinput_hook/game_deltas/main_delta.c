4d3bfbc3924f1016ad455d43afe0390651c17b9b
Timothee Denizou <timothee.denizou@epita.fr>
2025-01-20 21:24:36 +0100

    removed dependency on Window_Main

--- dinput_hook/game_deltas/main_delta.c
+++ dinput_hook/game_deltas/main_delta.c
@@ -3,14 +3,15 @@
 #include <windows.h>
 #include <stdint.h>
 
-#include "./Win95/Window.h"
+#include "./Window_delta.h"
 #include "macros.h"
 
 // 0x004238d0
 int __stdcall WinMain_delta(HINSTANCE hInstance, HINSTANCE hPrevInstance, PSTR pCmdLine,
                             int nCmdShow) {
     // TODO: have a cast of address for sub functions instead of a hook
-    Window_Main(hInstance, hPrevInstance, pCmdLine, nCmdShow, "Episode I Racer: Community Edition");
+    Window_Main_delta(hInstance, hPrevInstance, pCmdLine, nCmdShow,
+                      "Episode I Racer: Community Edition");
 
     return 0;
 }
